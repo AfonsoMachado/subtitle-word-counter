379ae992080278952330f07c73caf8ae7333f861
Afonso Machado <afonso777@hotmail.com>
2020-07-12 23:50:54 -0300

    ajustes e interface inicial do software

---
+++ src/components/Pill.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <span>{{ name }}</span>
+        <span>{{ amount }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    // Parametros a serem recebidos
+    props: ['name', 'amount']
+}
+</script>
+
+<style>
+
+</style>
